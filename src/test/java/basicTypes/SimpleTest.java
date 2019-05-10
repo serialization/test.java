69e9c041588936065f4072e5880bc38b9048dc55
Timm Felden <feldentm@informatik.uni-stuttgart.de>
2019-05-10 13:55:47 +0200

    explain test case

--- src/test/java/basicTypes/SimpleTest.java
+++ src/test/java/basicTypes/SimpleTest.java
@@ -8,42 +8,44 @@ import ogss.common.java.api.Mode;
 import ogss.common.java.internal.DynamicDataIterator;
 
 /**
+ * Ensure that SeqParser can read files with dropped blocks.
  * 
+ * @author Sarah Stieß, Timm Felden
  */
 @SuppressWarnings("static-method")
 public class SimpleTest extends common.CommonTest {
-    
-	/*
-	 * make 2^10 + 1 instances of T and set b for all but the last to false.
-	 * write file, read it back and ensure that exactly on b is of value true. 
-	 */
-	@Test
+
+    /**
+     * make 2^10 + 1 instances of T and set b for all but the last to false. write file, read it back and ensure that
+     * exactly on b is of value true.
+     */
+    @Test
     public void manyInstancesTest() throws Exception {
         OGFile sg = OGFile.open(tmpFile("make"), Mode.Create, Mode.Write);
-        
-        //2^20 = 1048576
-        for (int i = 0; i < 1048576; i++) 
-        	sg.BasicBools.make().setBasicBool(false);
+
+        // 2^20 = 1048576
+        for (int i = 0; i < 1048576; i++)
+            sg.BasicBools.make().setBasicBool(false);
         sg.BasicBools.make().setBasicBool(true);
         sg.close();
-        
+
         {
-        	OGFile sg1 = OGFile.open(sg.currentPath(), Mode.ReadOnly);
-        	
-        	DynamicDataIterator<BasicBool> it = sg1.BasicBools.iterator();
-        	
-        	Assert.assertEquals(1048577, sg1.BasicBools.size());
-        	
-        	int f = 0;
-        	while (it.hasNext()) {
-        		BasicBool t = it.next();
-        		if (t.getBasicBool())
-        			f++;
-        	}
-        	Assert.assertEquals(1, f);
-        	
+            OGFile sg1 = OGFile.open(sg.currentPath(), Mode.ReadOnly);
+
+            DynamicDataIterator<BasicBool> it = sg1.BasicBools.iterator();
+
+            Assert.assertEquals(1048577, sg1.BasicBools.size());
+
+            int f = 0;
+            while (it.hasNext()) {
+                BasicBool t = it.next();
+                if (t.getBasicBool())
+                    f++;
+            }
+            Assert.assertEquals(1, f);
+
         }
-        
+
     }
 
 }
